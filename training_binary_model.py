b2c988c4fedf14f237838f8596501e3464ad3845
seungtaek94 <seungtaek.kim.94@gmail.com>
2021-02-23 17:52:20 +0900

    update

---
+++ training_binary_model.py
@@ -0,0 +1,201 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']= '3'
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+
+import tensorflow as tf
+import time, logging
+import argparse
+from models.unetV2 import Unet
+from models.pspunet import pspunet
+from util.dataloader.cityscapes import get_dataset
+from util.func import *
+from util.losses import *
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # Training
+    parser.add_argument('--batch-size', type=int, default=8, help='training batch size per device (CPU/GPU).')
+    #parser.add_argument('--num-gpus', type=int, default=2, help='number of gpus to use.')
+    parser.add_argument('--model', type=str, default='unet',
+                        help='model to use. options are resnet and wrn. default is resnet.')
+    parser.add_argument('--num-epochs', type=int, default=10, help='number of training epochs.')
+    parser.add_argument('--lr', type=float, default=0.0001, help='learning rate. default is 0.1.')
+    parser.add_argument('--lr-decay', type=float, default=0.1, help='decay rate of learning rate. default is 0.1.')
+    parser.add_argument('--lr-decay-step', type=str, default='2,5',
+                        help='epochs at which learning rate decays. default is 80,120')
+
+    parser.add_argument('--resume-from', type=str, help='resume training from the model')
+    parser.add_argument('--optimizer', type=str, default='adam', help='sgd, nag')
+    parser.add_argument('--model-name', type=str, default='unet', help='model name')
+
+    # Dataset
+    parser.add_argument('--dataset-path', type=str, default='/home/seungtaek/ssd1/datasets/cityscapes',
+                        help='dataset path')
+    parser.add_argument('--image-height', type=int , default=512, help='image height')
+    parser.add_argument('--image-width', type=int, default=1024, help='image width')
+
+    return parser.parse_args()
+
+def decay(epoch, lr, lr_decay, lr_decay_step):
+    if epoch < lr_decay_step[0]:
+        return lr
+    elif lr_decay_step[0] <= epoch < lr_decay_step[1]:
+        return lr * lr_decay
+    else:
+        return lr * (lr_decay ** 2)
+
+
+def create_mask(pred_mask: tf.Tensor) -> tf.Tensor:
+    """Return a filter mask with the top 1 predictions
+    only.
+
+    Parameters
+    ----------
+    pred_mask : tf.Tensor
+        A [IMG_SIZE, IMG_SIZE, N_CLASS] tensor. For each pixel we have
+        N_CLASS values (vector) which represents the probability of the pixel
+        being these classes. Example: A pixel with the vector [0.0, 0.0, 1.0]
+        has been predicted class 2 with a probability of 100%.
+
+    Returns
+    -------
+    tf.Tensor
+        A [IMG_SIZE, IMG_SIZE, 1] mask with top 1 predictions
+        for each pixels.
+    """
+    # pred_mask -> [IMG_SIZE, SIZE, N_CLASS]
+    # 1 prediction for each class but we want the highest score only
+    # so we use argmax
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    # pred_mask becomes [IMG_SIZE, IMG_SIZE]
+    # but matplotlib needs [IMG_SIZE, IMG_SIZE, 1]
+    pred_mask = tf.expand_dims(pred_mask, axis=-1)
+    return pred_mask
+
+def show_predictions(img, gt, epoch, plot_dir):
+    """Show a sample prediction.
+
+    Parameters
+    ----------
+    dataset : [type], optional
+        [Input dataset, by default None
+    num : int, optional
+        Number of sample to show, by default 1
+    """
+    pred_mask = model.predict(img)
+    display_sample([img[0], gt[0], create_mask(pred_mask[0])], epoch, plot_dir)
+
+class DisplayCallback(tf.keras.callbacks.Callback):
+    def __init__(self, img, gt, plot_dir):
+        super(DisplayCallback, self).__init__()
+        self.img=img
+        self.gt=gt
+        self.plot_dir=plot_dir
+
+    def on_epoch_end(self, epoch, logs=None):
+        show_predictions(self.img, self.gt, epoch, self.plot_dir)
+        print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
+
+if __name__ == '__main__':
+    opt = parse_args()
+
+    strategy = tf.distribute.MirroredStrategy()
+
+    now = time.localtime(time.time())
+    timePrefix = f'{now.tm_year}{now.tm_mon:0>2}{now.tm_mday:0>2}{now.tm_hour:0>2}{now.tm_min:0>2}{now.tm_sec:0>2}'
+
+    ignore_classes = [8, 9, 10, 11, 12, 13, 15,
+                      16, 17, 18, 19, 21, 22, 23, 24, 25, 27,
+                      28, 29, 30, 31, 32, 33, -1]
+
+    #ignore_classes = [-1]
+
+    num_classes = 35 - len(ignore_classes) - 1
+
+    if not os.path.isdir('./log'):
+        os.mkdir('./log')
+
+    model_name = opt.model_name
+    filehandler = logging.FileHandler(f'./log/{model_name}_c{num_classes}_{timePrefix}.log')
+    streamhandler = logging.StreamHandler()
+
+    logger = logging.getLogger('')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(filehandler)
+    logger.addHandler(streamhandler)
+    logger.info(opt)
+    logger.info(f'#Classes: {num_classes}')
+
+    if not os.path.isdir('./exp'):
+        os.mkdir('./exp')
+
+    exp_base_dir = f'./exp/{opt.model_name}_{timePrefix}_c{num_classes}'
+    checkpoints_dir = exp_base_dir + '/checkpoints'
+    plot_dir = exp_base_dir + '/plot'
+
+    os.mkdir(exp_base_dir)
+    os.mkdir(checkpoints_dir)
+    os.mkdir(plot_dir)
+
+    logger.info(f'Exp Dir: {exp_base_dir}')
+
+    dataset = get_dataset(opt.dataset_path, ignore_classes, opt.image_height, opt.image_width, batch_size=opt.batch_size)
+
+    for image, mask in dataset['train'].take(1):
+        sample_image, sample_mask = image, mask
+
+    print("Train Shape: ", dataset['train'])
+    print("Val Shape:", dataset['val'])
+
+    with strategy.scope():
+        #model = Unet(opt.image_height, opt.image_width, num_classes)
+        model = pspunet((opt.image_height, opt.image_width, 3), num_classes)
+        train_ds = strategy.experimental_distribute_dataset(dataset['train'])
+        val_ds = strategy.experimental_distribute_dataset(dataset['val'])
+
+        if num_classes == 2:
+            loss_fn = soft_dice_loss(smooth=0.2)
+            #loss_fn = tf.keras.losses.BinaryCrossentropy()
+            logger.info("Loss: soft_dice_loss")
+        else:
+            #loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+            loss_fn = focal_loss()
+            logger.info("Loss: focal")
+
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=opt.lr),
+            loss=loss_fn,
+            metrics=['accuracy']
+        )
+
+    cp_prefix = os.path.join(checkpoints_dir, "ckpt_{epoch:0>3}.h5")
+
+    lr_decay_step = list(map(int, opt.lr_decay_step.split(',')))
+    tensorboar_logs_dir = f'{exp_base_dir}/tblogs'
+    logger.info(f'Tensorboard: /home/seungtaek/project/segmentation/{tensorboar_logs_dir}')
+
+    callbacks = [
+        DisplayCallback(sample_image, sample_mask, plot_dir),
+        tf.keras.callbacks.TensorBoard(log_dir=tensorboar_logs_dir),
+        tf.keras.callbacks.ModelCheckpoint(filepath=cp_prefix),
+        tf.keras.callbacks.LearningRateScheduler(lambda epoch: decay(epoch, opt.lr, opt.lr_decay, lr_decay_step))
+    ]
+
+    #steps_per_epoch = 2975 // opt.batch_size
+    #val_step = 500 // opt.batch_size
+
+    steps_per_epoch = 10
+    val_step = 1
+
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=opt.num_epochs,
+        steps_per_epoch=steps_per_epoch,
+        validation_steps=val_step,
+        callbacks=callbacks
+    )
+
+    model_save_dir = f'{exp_base_dir}/save_model'
+    os.mkdir(model_save_dir)
+    model.save(model_save_dir)
--- util/dataloader/cityscapes.py
+++ util/dataloader/cityscapes.py
@@ -1,25 +1,12 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import numpy as np
 import pathlib
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import time
 
-def parse_image(img_path: str) -> dict:
-    """Load an image and its annotation (mask) and returning
-    a dictionary.
-
-    Parameters
-    ----------
-    img_path : str
-        Image (not the mask) location.
-
-    Returns
-    -------
-    dict
-        Dictionary mapping an image and its annotation.
-    """
+def parse_image(img_path: str, ignore_class: list) -> dict:
     image = tf.io.read_file(img_path)
     image = tf.image.decode_png(image, channels=3)
     image = tf.image.convert_image_dtype(image, tf.uint8)
@@ -33,64 +20,24 @@ def parse_image(img_path: str) -> dict:
     mask = tf.io.read_file(mask_path)
     mask = tf.image.decode_png(mask, channels=1)
 
-
-    ''' 
-    for c in ignore_class:
-        mask = tf.where(mask == c, np.dtype('uint8').type(0), mask)
-    '''
-    mask = tf.where(mask != 7, np.dtype('uint8').type(0), mask)
-
-    #plt.figure(figsize=(10, 10))
-    #plt.imshow(mask, cmap='gray', vmin=0, vmax=10)
-    #plt.show()
+    if len(ignore_class) == 33:
+        mask = tf.where(mask != 7, np.dtype('uint8').type(0), mask)
+        mask = tf.where(mask == 7, np.dtype('uint8').type(1), mask)
+    else:
+        for c in ignore_class:
+            mask = tf.where(mask == c, np.dtype('uint8').type(34), mask)
 
     return {'image': image, 'segmentation_mask': mask}
 
-@tf.function
+#@tf.function
 def normalize(input_image: tf.Tensor, input_mask: tf.Tensor) -> tuple:
-    """Rescale the pixel values of the images between 0.0 and 1.0
-    compared to [0,255] originally.
-
-    Parameters
-    ----------
-    input_image : tf.Tensor
-        Tensorflow tensor containing an image of size [SIZE,SIZE,3].
-    input_mask : tf.Tensor
-        Tensorflow tensor containing an annotation of size [SIZE,SIZE,1].
-
-    Returns
-    -------
-    tuple
-        Normalized image and its annotation.
-    """
     input_image = tf.cast(input_image, tf.float32) / 255.0
     return input_image, input_mask
 
-@tf.function
-def load_image_train(datapoint: dict, img_hight=1024, img_width=2048) -> tuple:
-    """Apply some transformations to an input dictionary
-    containing a train image and its annotation.
-
-    Notes
-    -----
-    An annotation is a regular  channel image.
-    If a transformation such as rotation is applied to the image,
-    the same transformation has to be applied on the annotation also.
-
-    Parameters
-    ----------
-    datapoint : dict
-        A dict containing an image and its annotation.
-    img_hight : int
-    img_width : int
-
-    Returns
-    -------
-    tuple
-        A modified image and its annotation.
-    """
-    input_image = tf.image.resize(datapoint['image'], (img_hight, img_width))
-    input_mask = tf.image.resize(datapoint['segmentation_mask'], (img_hight, img_width))
+#@tf.function
+def load_image_train(datapoint: dict, img_height=1024, img_width=2048) -> tuple:
+    input_image = tf.image.resize(datapoint['image'], (img_height, img_width))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (img_height, img_width))
 
     if tf.random.uniform(()) > 0.5:
         input_image = tf.image.flip_left_right(input_image)
@@ -100,36 +47,17 @@ def load_image_train(datapoint: dict, img_hight=1024, img_width=2048) -> tuple:
 
     return input_image, input_mask
 
-@tf.function
-def load_image_test(datapoint: dict, img_hight=1024, img_width=2048) -> tuple:
-    """Normalize and resize a test image and its annotation.
-
-    Notes
-    -----
-    Since this is for the test set, we don't need to apply
-    any data augmentation technique.
-
-    Parameters
-    ----------
-    datapoint : dict
-        A dict containing an image and its annotation.
-    img_hight : int
-    img_width : int
-
-    Returns
-    -------
-    tuple
-        A modified image and its annotation.
-    """
-    input_image = tf.image.resize(datapoint['image'], (img_hight, img_width))
-    input_mask = tf.image.resize(datapoint['segmentation_mask'], (img_hight, img_width))
+#@tf.function
+def load_image_test(datapoint: dict, img_height=1024, img_width=2048) -> tuple:
+    input_image = tf.image.resize(datapoint['image'], (img_height, img_width))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (img_height, img_width))
 
     input_image, input_mask = normalize(input_image, input_mask)
 
     return input_image, input_mask
 
 
-def get_dataset(path: str, ignore_class: list, img_hight=1024, img_width=2048, batch_size=32) -> dict:
+def get_dataset(path: str, ignore_class: list, img_height=512, img_width=1024, batch_size=32) -> dict:
     AUTOTUNE = tf.data.experimental.AUTOTUNE
     BUFFER_SIZE = 1000
 
@@ -146,18 +74,18 @@ def get_dataset(path: str, ignore_class: list, img_hight=1024, img_width=2048, b
     print(f"The Validation Dataset contains {VALSET_SIZE} images.")
 
     train_ds = tf.data.Dataset.list_files(str(training_data_path + '/*/*.png'), shuffle=False, seed=123)
-    train_ds = train_ds.map(parse_image)
+    train_ds = train_ds.map(lambda x: parse_image(x, ignore_class))
 
     val_ds = tf.data.Dataset.list_files(str(val_data_path + '/*/*.png'), shuffle=False, seed=123)
-    val_ds = val_ds.map(parse_image)
+    val_ds = val_ds.map(lambda x: parse_image(x, ignore_class))
 
-    train_ds = train_ds.map(lambda x: load_image_train(x, img_hight, img_width), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    train_ds = train_ds.shuffle(buffer_size=BUFFER_SIZE, seed=123)
+    train_ds = train_ds.map(lambda x: load_image_train(x, img_height, img_width), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    #train_ds = train_ds.shuffle(buffer_size=BUFFER_SIZE, seed=123)
     train_ds = train_ds.repeat()
     train_ds = train_ds.batch(batch_size)
     train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
 
-    val_ds = val_ds.map(lambda x: load_image_test(x, img_hight, img_width))
+    val_ds = val_ds.map(lambda x: load_image_test(x, img_height, img_width))
     val_ds = val_ds.repeat()
     val_ds = val_ds.batch(batch_size)
     val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
@@ -168,10 +96,16 @@ def get_dataset(path: str, ignore_class: list, img_hight=1024, img_width=2048, b
 
 if __name__ == '__main__':
     data_path = '/home/seungtaek/ssd1/datasets/cityscapes'
-    ignore_class = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-                    29, 30, 31, 32, 33, -1]
-
+    ignore_class = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 29, 30, 31, 32, 33, -1]
     dataset = get_dataset(data_path, ignore_class, 1024, 2048, batch_size=32)
+
+    for image, mask in dataset['train'].take(1):
+        sample_image, sample_mask = image, mask
+
+    print(sample_image.shape, sample_mask.shape)
+
+    from util.func import display_sample
+    display_sample([sample_image[0], sample_mask[0]])
 
     print("Train: ", dataset['train'])
     print("Val: ", dataset['val'])
