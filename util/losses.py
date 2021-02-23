c7380c5b5443cf26d1aba51398e7e5960fcb0ebe
seungtaek94 <seungtaek.kim.94@gmail.com>
2021-02-23 17:50:34 +0900

    add focal loss and dice loss

---
+++ util/losses.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+
+def focal_loss(alpha=0.25, gamma=2):
+    def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
+        targets = tf.cast(targets, tf.float32)
+        weight_a = alpha * (1 - y_pred) ** gamma * targets
+        weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
+
+        return (tf.math.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (
+                    weight_a + weight_b) + logits * weight_b
+
+    def loss(y_true, logits):
+        y_pred = tf.math.sigmoid(logits)
+        loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
+
+        return tf.reduce_mean(loss)
+
+    return loss
+
+def dice_loss(y_true, y_pred):
+  y_true = tf.cast(y_true, tf.float32)
+  y_pred = tf.math.sigmoid(y_pred)
+  numerator = 2 * tf.reduce_sum(y_true * y_pred)
+  denominator = tf.reduce_sum(y_true + y_pred)
+
+  return 1 - numerator / denominator
+
+def soft_dice_loss(smooth=1.0):
+    def loss(y_true, y_pred):
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.math.sigmoid(y_pred)
+        numerator = 2 * tf.reduce_sum(y_true * y_pred) + smooth
+        denominator = tf.reduce_sum(y_true + y_pred) + smooth
+
+        return 1 - numerator / denominator
+
+    return loss
+
+
+
+def ce_dice_loss(y_true, y_pred):
+    def dice_loss(y_true, y_pred):
+      y_pred = tf.math.sigmoid(y_pred)
+      numerator = 2 * tf.reduce_sum(y_true * y_pred)
+      denominator = tf.reduce_sum(y_true + y_pred)
+
+      return 1 - numerator / denominator
+
+    y_true = tf.cast(y_true, tf.float32)
+    o = tf.nn.sigmoid_cross_entropy_with_logits(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return tf.reduce_mean(o)
